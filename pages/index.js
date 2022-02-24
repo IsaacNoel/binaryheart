81a0b06fad7e464d6a396a10bfb0267894140966
malachinoel3 <malachidnoel@gmail.com>
2022-02-24 16:01:56 -0600

    took out footer

--- pages/index.js
+++ pages/index.js
@@ -41,11 +41,11 @@ export default function Home() {
           <Link href="/about">
           <a className={styles.card}>
             <h2>About Us &rarr;</h2>
-            <p>See our mission, what we&aspos;ve done, and who we are</p>
+            <p>See our mission, what we&lsquo;ve done, and who we are</p>
           </a>
         </Link>
 
-        <Link href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app">
+        <Link href="/contact">
           <a
             className={styles.card}
           >
@@ -59,16 +59,7 @@ export default function Home() {
       </main>
 
       <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
+
       </footer>
     </div>
   )
