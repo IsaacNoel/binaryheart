e30ab02d9495f5a5f0c05dc9625e1a4c01c62074
malachinoel3 <malachidnoel@gmail.com>
2022-02-24 16:39:16 -0600

    mobile_test

--- pages/donate.js
+++ pages/donate.js
@@ -52,7 +52,11 @@ export default function Home() {
                 </MDBNavbar>
             </header>
             <Head>
-
+                <title>Create Next App</title>
+                <link rel="icon" href="/favicon.ico" />
+                <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css"
+                      integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4"
+                      crossOrigin="anonymous"/>
             </Head>
         </div>
     )
