e5122ca22f09efb32aa9cfe2a8a405e56f0be6f1
malachinoel3 <malachidnoel@gmail.com>
2022-03-03 16:09:55 -0600

    header font size & donate page

--- pages/about.js
+++ pages/about.js
@@ -10,7 +10,7 @@ import {
     MDBContainer,
     MDBIcon
 } from 'mdb-react-ui-kit';
-import Link from "next/link"
+import Link from "next/link";
 
 export default function Home() {
     return (
@@ -27,7 +27,7 @@ export default function Home() {
                         >
                             <MDBIcon fas icon='bars' />
                         </MDBNavbarToggler>
-                        <div className='collapse navbar-collapse' id='navbarExample01'>
+                        <div className='collapse navbar-collapse headerdiv' id='navbarExample01'>
                             <MDBNavbarNav right className='mb-2 mb-lg-0'>
                                 <MDBNavbarItem active>
                                     <MDBNavbarLink aria-current='page' href='/'>
@@ -61,24 +61,24 @@ export default function Home() {
             </Head>
 
             <main className={styles.main}>
-                
-                    
+
+
                 <h2 className={styles.semititle}>
-                    Hi!
+                    About Us
                 </h2>
 
                 <p className={styles.description}>
-                    BinaryHeart is a student organization whose objective 
-                    is to <code className={styles.code}>spread Digital 
-                    Access</code>. We collect donations of broken or 
-                    used electronic devices and repair them. Through 
-                    Northwestern Settlement and other partner charities, 
-                    the devices are donated to help our less privileged 
-                    peers. Our hope is that our work will lift people out 
-                    of poverty, make higher education a reality for more 
+                    BinaryHeart is a student organization whose objective
+                    is to <code className={styles.code}>spread Digital
+                    Access</code>. We collect donations of broken or
+                    used electronic devices and repair them. Through
+                    Northwestern Settlement and other partner charities,
+                    the devices are donated to help our less privileged
+                    peers. Our hope is that our work will lift people out
+                    of poverty, make higher education a reality for more
                     students, and change lives.
-                </p>       
-                
+                </p>
+
                 <div
                     className ={styles.static_card}
                     style={{
@@ -86,10 +86,8 @@ export default function Home() {
                         "background-color": "rgb(255, 240, 243)"
                     }}
                 >
-                    <h1 style={{
-                        "text-align": "center"
-                    }}>
-                        We&apos;ve done some cool stuff
+                    <h1>
+                        <center>We&lsquo;ve done some cool stuff</center>
                     </h1>
                     <p className={styles.description}>
                         We have... donated to some cool places, including Kenya. Check out these pictures!
@@ -97,9 +95,8 @@ export default function Home() {
                     </p>
                     <div className={styles.grid}>
 
-                        <div className={styles.scalingimage}>
-                        <Image alt="Cool pic" layout={"responsive"} height={"100%"} width={"100%"} src="/656348main_ToV_transit_diag_full.jpg" />
-                        </div>
+                        <img className={styles.scalingimage} src="/656348main_ToV_transit_diag_full.jpg" />
+
                         <p className={styles.description}>
                             asdf
                         </p>
@@ -113,27 +110,52 @@ export default function Home() {
                         </p>
                     </div>
                 </div>
-               
+
                 <div className ={styles.static_card}>
                     <h1>
-                        <center>Student leaders</center>
+                        <center>Student Leaders</center>
                     </h1>
 
                     <p className={styles.description}>
-                        We lead our team, securing donations and reaching out to those in need. We own seventeen cats, 4 of whom are named Cindy.
+                        <b> Maryanne Xu</b>
+                        <p>Hi everyone! I&lsquo;m a senior at New Trier High School.
+                            I volunteered for BinaryHeart for four years.
+                            During club meetings, I allocate tasks, run club logistics, and work on this website!
+                            I can hold my breath for 2 hours.</p>
+                        <b> Nicholas Sutherland</b>
+                        <p> Howdy! I&lsquo;m Nicholas, a senior at New Trier High School,
+                            and this is my third year at BinaryHeart. I usually end up building shelves,
+                            organizing screwdrivers, and working on our workspace during club.
+                            Oh yeah, and I invented the microwave burrito.</p>
+                        <b> Rom Fradkin</b>
+                        <p> Rom Fradkin (5’ 9’’, 150lbs) is a senior at New Trier High School.
+                            He&lsquo;s volunteered for BinaryHeart since freshman year. He owns four cows.
+                            </p>
+                        <b> Julia Hudson </b>
+                        <p> Hey! I&lsquo;m Julia, a sophomore at New Trier High School.
+                            I&lsquo;ve volunteered for BinaryHeart for two years now.
+                            When not fixing computers, I work on our website,
+                            manage our inventory system, and have plans for chapter expansion.
+                            </p>
+                        <b> Andrew Gill </b>
+                        <p> Hello, I&lsquo;m Andrew! :) I&lsquo;m a senior at New Trier High School,
+                            and I&lsquo;ve been in the club since my sophomore year.
+                            I&lsquo;m usually in charge of organization, whether on the task sheet or
+                            in the closet I spent an entire year setting up.
+                            By the way, I&lsquo;ve been to the North Pole!
+                            </p>
                     </p>
                 </div>
+
                 <Link href="/">
-                <a className={styles.card}>
-                     <h2>Back to the launch page &rarr;</h2>
-                 </a>
+                    <a className={styles.card}>
+                        <h2>Back to the launch page &rarr;</h2>
+                    </a>
                 </Link>
             </main>
 
 
-            <footer className={styles.footer}>
 
-            </footer>
         </div>
     )
 }
