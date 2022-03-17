42d2780db505ee6f448def10a318721fe467345e
Isaac Noel <isaacnoel@Isaacs-MacBook-Pro.local>
2022-03-17 16:47:27 -0500

    remove error

---
+++ pages/newmembers.js
@@ -0,0 +1,12 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+import Link from 'next/link'
+
+export default function Home() {
+    return (
+        <div className={styles.container}>
+           
+        </div>
+    )
+}
