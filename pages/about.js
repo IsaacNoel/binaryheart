6bb5d1016bf0c2b426becdfc5207bb70b54064d8
Isaac Noel <isaacnoel@Isaacs-MacBook-Pro.local>
2022-03-17 17:00:21 -0500

    added alts

--- pages/about.js
+++ pages/about.js
@@ -141,7 +141,7 @@ export default function Home() {
                         <table>
                             <tr>
                                 <td width="25%">
-                                    <img className={styles.scalingimage} src="/maryanne.jpg" />
+                                    <img className={styles.scalingimage} src="/maryanne.jpg" alt="Maryanne" />
                                 </td>
                                 <td>
                                     <b> Maryanne Xu </b>
@@ -153,7 +153,7 @@ export default function Home() {
                             </tr>
                             <tr>
                                 <td width="25%">
-                                    <img className={styles.scalingimage} src="/nicholas.jpg" />
+                                    <img className={styles.scalingimage} src="/nicholas.jpg" alt="Nicholas" />
                                 </td>
                                 <td>
                                     <b> Nicholas Sutherland </b>
@@ -188,7 +188,7 @@ export default function Home() {
                             </tr>
                             <tr>
                                 <td width="25%">
-                                    <img className={styles.scalingimage} src="/andrew.jpg" />
+                                    <img className={styles.scalingimage} src="/andrew.jpg" alt="Andrew" />
                                 </td>
                                 <td>
                                     <b> Andrew Gill </b>
