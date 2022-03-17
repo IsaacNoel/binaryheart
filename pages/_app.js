1acc93f6b01804eff31655ddf1cd36c307c5a11c
TetrisLitHub <77713718+TetrisLitHub@users.noreply.github.com>
2022-03-17 17:16:07 -0500

    Change webpage title and description, attempted to update favicon.ico (little image)

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,10 @@
 import '../styles/globals.css'
+import {Head} from "next/document";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return(
+      <Component {...pageProps} />
+  )
 }
 
 export default MyApp
