a8c4dbee46a88ba7a45b662595abe17d39070f62
malachinoel3 <malachidnoel@gmail.com>
2022-04-14 17:01:22 -0500

    meta tags

--- pages/index.js
+++ pages/index.js
@@ -5,64 +5,70 @@ import Link from 'next/link'
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>BinaryHeart</title>
-        <meta name="description" content="BinaryHeart official website, from Create Next App" />
-        <link rel="icon" href="/images/favicon.ico" />
-        <link rel="apple-touch-icon" href="/favpng.png" />
-      </Head>
+      <div className={styles.container}>
+        <Head>
+          <title>BinaryHeart</title>
+          <meta name="description" content="Bridging the digital divide" />
+          <link rel="icon" href="/images/favicon.ico" />
+          <link rel="apple-touch-icon" href="/favpng.png" />
+          <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css"
+                integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4"
+                crossOrigin="anonymous"/>
+          <meta name="Description"
+                content="Author: Malachi Noel, Maryanne Xu, Artharva Metkar, Megan Lu, Isaac Noel"/>
+        </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <font color = "#ff0040">Binary</font><font color = "#193961">Heart</font>!
-        </h1>
-      
-        <p className={styles.description}>
-          BinaryHeart is an after-school nonprofit that repairs donated technology and re-donates those devices to low-income schools.
-          If you are a New Trier student, <Link href="/newmembers"> get involved here</Link>!
-          
-          
-        </p>
+        <main className={styles.main}>
+          <h1 className={styles.title}>
+            Welcome to <font color = "#ff0040">Binary</font><font color = "#193961">Heart</font>!
+          </h1>
 
-        <div className={styles.grid}>
-          <Link href="/products">
-            <a className={styles.card}>
-              <h2>Availabe Pruducts &rarr;</h2>
-              <p>See the solutions we have to offer</p>
-            </a>
-          </Link>
+          <p className={styles.description}>
+            BinaryHeart is an after-school nonprofit that repairs donated technology and re-donates those devices to low-income schools.
+            If you are a New Trier student, <Link href="/newmembers"> get involved here</Link>!
 
-          <Link href="/press">
-          <a className={styles.card}>
-            <h2>Press &rarr;</h2>
-            <p>See our press coverage in the news</p>
-          </a>
-          </Link>
 
-          <Link href="/about">
-          <a className={styles.card}>
-            <h2>About Us &rarr;</h2>
-            <p>See our mission, what we&lsquo;ve done, and who we are</p>
-          </a>
-        </Link>
+          </p>
 
-        <Link href="/contact">
-          <a
-            className={styles.card}
-          >
-            <h2>Contact Us &rarr;</h2>
-            <p>
-              Want to donate or receive old tech? Get in contact!
-            </p>
-          </a>
-        </Link>
-        </div>
-      </main>
+          <div className={styles.grid}>
+            <Link href="/press">
+              <a className={styles.card}>
+                <h2>Press &rarr;</h2>
+                <p>See our press coverage in the news</p>
+              </a>
+            </Link>
 
-      <footer className={styles.footer}>
+            <Link href="/donate">
+              <a className={styles.card}>
+                <h2>Donate &rarr;</h2>
+                <p>Donate your old or unused tech</p>
+              </a>
+            </Link>
 
-      </footer>
-    </div>
+            <Link href="/contact">
+              <a
+                  className={styles.card}
+              >
+                <h2>Contact Us &rarr;</h2>
+                <p>
+                  Want to receive old tech? Get in contact!
+                </p>
+              </a>
+            </Link>
+
+            <Link href="/about">
+              <a className={styles.card}>
+                <h2>About&rarr;</h2>
+                <p>See our mission, what we&lsquo;ve done, and who we are</p>
+              </a>
+            </Link>
+
+          </div>
+        </main>
+
+        <footer className={styles.footer}>
+
+        </footer>
+      </div>
   )
 }
