64701523ef0d15d2b079e286e3c1365d1762388d
Megan Lu <mklu847@gmail.com>
2022-04-15 14:32:12 -0500

    i switched the wrong thing

--- pages/index.js
+++ pages/index.js
@@ -67,8 +67,8 @@ export default function Home() {
         <Link href="https://www.instagram.com/binaryheartnt/"><img className={styles.scalingimageicon} src="/instagram.png" /></Link>
         <img className={styles.scalingimageicon} src={"/blank space.png"} />
         <Link href="https://www.linkedin.com/company/binaryheart/"><img className={styles.scalingimageicon} src="/linkedin icon.png" /></Link>
-        <img className={styles.scalingimageemail} src={"/blank space.png"} />
-        <Link href="https://mail.google.com/mail/?view=cm&fs=1&to=contact@binaryheart.org"><img className={styles.scalingimageicon} src="/email.png" /></Link>
+        <img className={styles.scalingimageicon} src={"/blank space.png"} />
+        <Link href="https://mail.google.com/mail/?view=cm&fs=1&to=contact@binaryheart.org"><img className={styles.scalingimageemail} src="/email.png" /></Link>
         <img className={styles.scalingimageicon} src={"/blank space.png"} />
         <Link href="tel:+1-847-910-7044"><img className={styles.scalingimageicon} src="/phone.png" /></Link>
       </footer>
