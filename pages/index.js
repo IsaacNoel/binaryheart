3ee60e854b8321269ac9bc6cadf7924c35e7606b
Megan Lu <mklu847@gmail.com>
2022-04-15 15:42:34 -0500

    hover over social media icons

--- pages/index.js
+++ pages/index.js
@@ -67,7 +67,7 @@ export default function Home() {
         <div className={styles.footertext}>
           <h2><font color = "#ff0040">Binary</font><font color = "#193961">Heart</font></h2>
           <p><a href="https://mail.google.com/mail/?view=cm&fs=1&to=contact@binaryheart.org">contact@binaryheart.org</a></p>
-          <p><a href="tel:+1-847-910-7044">Phone: (847)910-7044</a></p>
+          <p>Phone: <a href="tel:+1-847-910-7044">(847)910-7044</a></p>
         </div>
         <div className={styles.footericons}>
           <Link href="https://www.instagram.com/binaryheartnt/"><img className={styles.scalingimageicon} src="/instagram.png" /></Link>
