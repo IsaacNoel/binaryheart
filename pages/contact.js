7769fc680d0d7883959fc50d526f20f95ededbdf
TetrisLitHub <77713718+TetrisLitHub@users.noreply.github.com>
2022-04-28 16:27:20 -0500

    added google form to contact.js + extra css for later

--- pages/contact.js
+++ pages/contact.js
@@ -65,6 +65,10 @@ export default function Home() {
                     Email us about questions and donations at <a href="https://mail.google.com/mail/?view=cm&fs=1&to=contact@binaryheart.org">contact@binaryheart.org (Gmail Link)</a><br/>
                     Also see: <Link href="/faqs">Frequently asked questions</Link>
                 </p>
+                <iframe
+                    src="https://docs.google.com/forms/d/e/1FAIpQLScG1w-t5g9lkYdlXtxsyYcazWpu8GuuzkHM_GtQO9th8WVjPQ/viewform?embedded=true"
+                    width="1000" height="1000" frameBorder="0" marginHeight="0" marginWidth="0">Loading…
+                </iframe>
                 <p className={styles.container}>
                     If you have any questions about the website itself, you can also find the developers at our <a href={"https://github.com/IsaacNoel/binaryheart"}>GitHub page</a>
                 </p>
